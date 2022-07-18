73dfdfb66878184cf9306b35f35de5079c9b890c
Bradley <bradbailey972@gmail.com>
2022-07-18 08:20:18 +0200

    Experimentation

--- src/helpers/perlin.py
+++ src/helpers/perlin.py
@@ -9,15 +9,56 @@ import numpy as np
 import random
 from noise import pnoise1
 
-WIDTH, HEIGHT = SIZE = (400, 400)
+WIDTH, HEIGHT = SIZE = (160, 90)
 SEED = random.randint(0, 100)
-print(SEED)
 
 
+class PerlinNoiseMap:
+    """
+    Perlin Noise map based off of https://pastebin.com/xnbsYSSw
+    """
 
+    def __init__(self, map_size):
+        self.tileset = {}
+        self.tile_groups = {}
 
+        self.prefab_plains = None
+        self.prefab_forest = None
+        self.prefab_hills = None
+        self.prefab_mountains = None
 
+        self.map_width, self.map_height = map_size
+        self.noise_grid = np.zeros(map_size)
+        self.tile_grid = np.zeros(map_size)
+
+        # recommend 4 to 20
+        self.maginification = 7.0
+
+        self.x_offset = 0
+        self.y_offset = 0
+
+    def Start(self):
+        self.CreateTileset()
+        self.CreateTileGroups()
+        self.GenerateMap()
+
+    def CreateTileset(self):
+        """Collect and assign ID codes to the tile prefabs, for ease of access.
+            Best ordered to match land elevation."""
+        self.tileset[0] = self.prefab_plains
+        self.tileset[1] = self.prefab_forest
+        self.tileset[2] = self.prefab_hills
+        self.tileset[3] = self.prefab_mountains
+
+    def CreateTileGroups(self):
+        """Create empty gameobjects for grouping tiles of the same type, ie
+            forest tiles"""
+        for prefab_pair in self.tileset.items():
+            pass
+
+    def GenerateMap(self):
+        pass
 
 
 if __name__ == '__main__':
-    pass
+    PerlinNoiseMap(SIZE)
